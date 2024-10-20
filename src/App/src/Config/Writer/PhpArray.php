01950eaa0afd2313760e976358cb6b54faec13eb
Joey Smith <jsmith@webinertia.net>
2024-10-20 14:50:59 -0500

    Adds laminas-config as dep Adds App\Config\Writer\PhpArray Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ src/App/src/Config/Writer/PhpArray.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config\Writer;
+
+use Laminas\Config\Writer\PhpArray as BaseWriter;
+
+use function sprintf;
+
+class PhpArray extends BaseWriter
+{
+    public final const TEMPLATE = <<<'EOT'
+        <?php
+
+        declare(strict_types=1);
+
+        return %s
+            %s
+        %s;
+
+        EOT;
+
+        public function processConfig(array $config)
+    {
+        $arraySyntax = [
+            'open'  => $this->useBracketArraySyntax ? '[' : 'array(',
+            'close' => $this->useBracketArraySyntax ? ']' : ')',
+        ];
+
+        return sprintf(
+            self::TEMPLATE,
+            $arraySyntax['open'],
+            $this->processIndented($config, $arraySyntax),
+            $arraySyntax['close']
+        );
+    }
+}
